523c0dd6cb725f1f5fcd0a6b19f6e11c5025927b
Aline Santos Lima <asantoli@everis.com>
2020-10-21 16:25:15 -0300

    alterações no programa

--- JavaAula1/src/TestePrimeiroPrograma.java
+++ JavaAula1/src/TestePrimeiroPrograma.java
@@ -3,5 +3,50 @@ public class TestePrimeiroPrograma {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		
+//		int idade = 15;
+//		
+//		if(idade >=18) {
+//			System.out.println("Possui permissão");
+//		} else {
+//			System.out.println("Não possui permissão");
+//		}
+		
+//		int i = 0;
+//		while(i <=10) {
+//			System.out.println(i);
+//			i+=1;
+//		}
+		
+//		for(int contador=0; contador<=10; contador++) {
+//			System.out.println(contador);
+//		}
+		
+//		int idadeAlunos = 18;
+//				
+//		switch(idadeAlunos) {
+//			case 18:
+//				System.out.println("Podem realizar o curso");
+//				break;
+//			case 15:
+//				System.out.println("Não podem realizar o curso");
+//				break;
+//			default:
+//				System.out.println("Nenhuma das opções acima");
+//				break;
+//		}
+		
+		int i = 0;
+		do{
+			System.out.println(i);
+			i++;
+		} while(i<=5);
+		
+		
+		
+		
+		
+		
 	}
 }
